94d1faaa51de2859b06784f3ef53c8dac5779a5e
asatk <asa15@rice.edu>
2025-08-30 20:48:36 -0500

    render animation much faster

--- plot.py
+++ plot.py
@@ -58,25 +58,34 @@ def plot_lat(phi: np.ndarray, theta: np.ndarray, flux: np.ndarray, nflux: int,
     lat_flux = np.sum(h_flux, axis=0)
 
 
-def anim_syn(synoptic_all: np.ndarray, dt: float, phibins: int=360, thetabins: int=180):
+def anim_syn(synoptic_all: np.ndarray, dt: float, phibins: int=360,
+             thetabins: int=180, flux_thresh: int=6.0, ms: int=100,
+             show: bool=True):
 
-    figa, ax = plt.subplots(nrows=1, ncols=1)
+    figa, ax = plt.subplots(nrows=1, ncols=1, figsize=(6,3))
 
-    im = ax.imshow(synoptic_all[0].T, cmap="gray", vmin=-100.0, vmax=100.0)
+    smap = np.transpose(synoptic_all, axes=(0,2,1))
+
+    im = ax.imshow(smap[0], cmap="gray", vmin=-flux_thresh, vmax=flux_thresh,
+                   extent=(0,phibins,0,thetabins))
+    title = ax.set_title("Frame {0:05d} ({t * dt / 86400:.01f} d)")
     ax.set_xticks([0, phibins//2, phibins-1], labels=["0", r"$\pi$", r"$2\pi$"])
     ax.set_xlabel(r"Azimuth $\phi$")
     ax.set_yticks([thetabins-1, thetabins//2, 0], labels=[r"$\pi$", r"$\pi/2$", "0"])
     ax.set_ylabel(r"Colatitude $\theta$")
-    cb = plt.colorbar(im, shrink=0.6, cmap="gray")
-    figtitle = figa.suptitle("Frame 0 ({t * dt / 86400:.01f} d)")
+    cb = plt.colorbar(im, shrink=0.9, cmap="gray", label=r"Flux ($10^{18}$ Mx)")
+    figa.tight_layout()
 
     def _update(t):
-        im = ax.imshow(synoptic_all[t].T, cmap="gray", vmin=-6.0, vmax=6.0)
-        figtitle.set_text(f"Frame {t} ({t * dt / 86400:.01f} d)")
-        return im, figtitle
+        im.set(data=smap[t])
+        title.set_text(f"Frame {t:05d} ({t * dt / 86400:.01f} d)")
+        return im, title
 
-    ms = 100
-    ani = anim.FuncAnimation(fig=figa, func=_update, frames=len(synoptic_all),
-                             interval=ms)
+    ani = anim.FuncAnimation(fig=figa, func=_update, frames=len(smap),
+                             interval=ms, blit=True)
+
     ani.save(filename="maps.gif", writer="pillow")
-    # plt.show()
+
+    if show:
+        plt.show()
+
